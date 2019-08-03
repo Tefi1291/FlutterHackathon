542cabf80d0a8a48d54344254a020463089f477e
notebook <pablofgarzon@gmail.com>
2019-08-03 01:32:14 -0300

    firsts steps in questionDetail UI

--- fly_code/lib/searchDetail.dart
+++ fly_code/lib/searchDetail.dart
@@ -21,11 +21,65 @@ class SearchDetail extends StatelessWidget
         title: Text("FlyCode"),
       ),
       body: Container(
-        child: HtmlView(
-          data: this.question.body,
-          stylingOptions: null,
-        ),
-      ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(this.question.owner.image),
+                        backgroundColor: Colors.transparent,
+                        radius: 20,
+                      ),
+                      Container(
+                        width: 100,
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Column(
+                          children: <Widget>[
+                            Text("Votes:"),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(Icons.thumb_up),
+                                Text(this.question.score.toString())
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Column(
+                            children: <Widget>[
+                              Text(this.question.owner.name),
+                              Text("Reputation: ${this.question.owner.reputation}"),
+                            ],
+                          ),
+                        )
+                      )
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text(this.question.title),
+                  ),
+                ],
+              ),
+              color: Colors.lightBlueAccent,
+            ),
+            Expanded(
+              child: HtmlView(
+                data: this.question.body,
+                stylingOptions: null,
+              ),
+            )
+          ],
+        )
+    ),
     );
   }
 }
